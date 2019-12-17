bdd15bb1e983c937f34200ffa258bc19a632449d
Jeremy White <jwhite@codeweavers.com>
2019-12-16 20:58:59 -0600

    Make game pylint clean.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 .PHONY: lint
 
-lint: ta.py
+lint: ta.py game.py
 	pylint3 --extension-pkg-whitelist=pygame $^
--- game.py
+++ game.py
@@ -1,46 +1,54 @@
 #!/usr/bin/python3
-import board
+""" game.py contains Game control logic for Trans-America """
 import random
 import copy
-import graphics
+import board
 
 class Game:
     ''' class for running a single game. '''
-    features=None
+    features = None
 
-    def __init__(self,players,features, inboard=None,hands=None):
-        self.features=features
-        self.hands={}
-        self.board=None
-        self.players=players
-        hubs=[]
+    def __init__(self, players, features, inboard=None, hands=None):
+        self.features = features
+        self.hands = {}
+        self.board = None
+        self.players = players
+        hubs = []
         for player in self.players:
-            self.hands[player[0]]={}
-            if(len(player)>2):
+            self.hands[player[0]] = {}
+            if len(player) > 2:
                 hubs.append(player[2])
-        if(inboard==None):
-            self.board=board.grid(features,len(players),hubs)
+        if inboard is None:
+            self.board = board.grid(features, len(players), hubs)
         else:
-            self.board=inboard
-        for key in self.features.cities.keys():
-            values=[]
-            for i in self.features.cities[key].keys():
-                values.append(i)
-            cities=random.sample(values,len(self.players))
-            for i in range(0,len(self.players)):
-                self.hands[players[i][0]][cities[i]]=self.features.cities[key][cities[i]]
-        for i in range(0,len(self.players)):
-            self.players[i][1]=self.players[i][1].init(copy.deepcopy(self.board),self.features,self.players[i][0],self.hands)
-    
+            self.board = inboard
+        if hands is None:
+            for key in self.features.cities.keys():
+                values = []
+                for i in self.features.cities[key].keys():
+                    values.append(i)
+                cities = random.sample(values, len(self.players))
+                for i in range(0, len(self.players)):
+                    self.hands[players[i][0]][cities[i]] = self.features.cities[key][cities[i]]
+        else:
+            self.hands = hands
+        for i in range(0, len(self.players)):
+            self.players[i][1] = self.players[i][1].init(copy.deepcopy(self.board),
+                                                         self.features, self.players[i][0],
+                                                         self.hands)
+
     def take_turn(self):
-        move=self.players[self.board.turn][1].move(copy.deepcopy(self.board))
+        """ Take exactly one turn """
+        move = self.players[self.board.turn][1].move(copy.deepcopy(self.board))
         self.board.make_move(move, self.board.turn)
         return self.board.is_terminal(self.hands)
-        
+
     def make_move(self, move, player):
+        """ Make a move for one player """
         return self.board.make_move(move, player)
 
     def play_game(self):
+        """ Play a whole game """
         while not self.board.is_terminal(self.hands):
             self.take_turn()
         return self.board.value(self.hands)
